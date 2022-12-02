27ac486f3894eef8691c2fd90c32bbef9da15af6
Eden <gresseden@gmail.com>
2022-12-02 16:32:14 -0500

    Fixing routes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Index from "./layout/Index";
 import { Routes, Route } from "react-router-dom";
 import About from "./views/About";
 import Menu from "./views/Menu";
+import Contact from "./views/Contact";
 import Undefined from "./views/Undefined";
 
 function App() {
@@ -12,8 +13,9 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Index />}></Route>
         <Route path="/about" element={<About />} />
-        <Route path="*" element={<Undefined />} />
         <Route path="/menu" element={<Menu />} />
+        <Route path="*" element={<Undefined />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
       {/* <Index /> */}
     </div>
